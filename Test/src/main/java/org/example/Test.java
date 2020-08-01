bb4ac62308bd6ce33eff9f7adc4aff826dacb634
housheng <housheng_xiao@foxmail.com>
2020-08-01 16:16:18 +0800

    git checkout branch

---
+++ Test/src/main/java/org/example/Test.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+
+/**
+ * @Description git命令使用
+ * @authror xiaohs
+ * @date 2020/8/1 16:13
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println();
+        ArrayList<Object> list = new ArrayList<>();
+        list.add("1");
+        list.add(1);
+
+    }
+}
